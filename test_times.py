f858551402feafd8228b80836ca4b81a2956ec16
nkhalilgharibi <nargess.khalilgharibi.11@ucl.ac.uk>
2021-04-30 15:22:02 +0100

    Add workshop timing test. Answers UCL-RITS/ipls-workshop#5

--- test_times.py
+++ test_times.py
@@ -7,7 +7,9 @@ def test_compute_overlap_within():
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
 
-def test_workshop_morning_times():
-    # replace ... with your code for the additional test here
-    # the structure should be very similar to the test above!
-    ...
+def test_workshop_nonoverlapping():
+    long_time_range = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short_time_range = time_range("2010-01-12 13:00:00", "2010-01-12 15:00:00", 2, 60)
+    result = compute_overlap_time(long_time_range, short_time_range)
+    expected = []
+    assert result == expected
